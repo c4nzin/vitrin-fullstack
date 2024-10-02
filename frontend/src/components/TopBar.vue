40fdbee3db4bbb3257a04dc20349fe13c62c15a3
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-10-02 15:11:02 +0300

    fix

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -10,6 +10,20 @@
         class="w-full p-3 pl-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         @keyup.enter="performSearch"
       />
+      <svg
+        class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M11 4a7 7 0 100 14 7 7 0 000-14zM21 21l-4.35-4.35"
+        />
+      </svg>
     </div>
 
     <div class="relative">
