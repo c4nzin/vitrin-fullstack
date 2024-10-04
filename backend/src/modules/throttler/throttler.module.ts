cc7cd617756e353efd4a5baab34b65caf9efd819
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-10-04 15:56:34 +0300

    add global decorator to throttler

--- backend/src/modules/throttler/throttler.module.ts
+++ backend/src/modules/throttler/throttler.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ThrottlerModule as RootThrottlerModule } from '@nestjs/throttler';
 import { ThrottlerConfigService } from './throttler-config.service';
 
+@Global()
 @Module({
   imports: [RootThrottlerModule.forRootAsync({ useClass: ThrottlerConfigService })],
 })
