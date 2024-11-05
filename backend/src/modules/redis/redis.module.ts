aeafe1b183182323ee079d29becf0cbc71314a3a
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-05 21:58:29 +0300

    Revert "CHECKPOINT"
    This reverts commit 537384cc60a262ee649331761e4b96085e1b4ab8.

--- backend/src/modules/redis/redis.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisConfigFactory } from './services/redis-config.service';
-import { CacheModule } from '@nestjs/cache-manager';
-
-@Module({
-  imports: [CacheModule.registerAsync({ useClass: RedisConfigFactory })],
-})
-export class RedisModule {}
