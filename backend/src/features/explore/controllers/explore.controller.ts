2a66ab3731c407ed0135d0c3c82c4c181fad30a3
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-05 14:15:44 +0300

    fix(backend): add more logic to explore controller

--- backend/src/features/explore/controllers/explore.controller.ts
+++ backend/src/features/explore/controllers/explore.controller.ts
@@ -6,17 +6,33 @@ import { AuthenticatedGuard } from 'src/common/guards';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { EXPLORE_CACHE_KEY } from '../constants';
+import { QueryBus } from '@nestjs/cqrs';
+import { ExploreCommand } from '../command/explore.command';
 
 @ApiTags('explore')
 @Controller('explore')
 @UseGuards(AuthenticatedGuard)
 export class ExploreController {
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Get()
   @Message('Sucessfully fetched posts.')
   @HttpCode(HttpStatus.OK)
   public async fetchPosts(): Promise<PostDocument[]> {
-    return this.cacheManager.get<PostDocument[]>(EXPLORE_CACHE_KEY);
+    const cachedPosts = await this.cacheManager.get<PostDocument[]>(EXPLORE_CACHE_KEY);
+
+    if (cachedPosts) {
+      return cachedPosts;
+    }
+
+    //bu yapı biraz mantıksız geldi incele.
+    const posts = await this.queryBus.execute(new ExploreCommand());
+
+    await this.cacheManager.set(EXPLORE_CACHE_KEY, posts, 600);
+
+    return posts;
   }
 }
