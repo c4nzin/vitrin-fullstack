4ae6f0584867350a452f1fdbd3d06bddf2ecb6aa
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-05 15:01:06 +0300

    refactor(backend): improve redis on explore feature

--- backend/src/features/explore/controllers/explore.controller.ts
+++ backend/src/features/explore/controllers/explore.controller.ts
@@ -1,38 +1,21 @@
 import { Controller, Get, HttpCode, HttpStatus, Inject, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Message } from 'src/common/decorators';
-import { PostDocument } from 'src/features/posts/schemas/post.schema';
-import { AuthenticatedGuard } from 'src/common/guards';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { EXPLORE_CACHE_KEY } from '../constants';
-import { QueryBus } from '@nestjs/cqrs';
-import { ExploreCommand } from '../command/explore.command';
+import { AuthenticatedGuard } from 'src/common/guards';
 
 @ApiTags('explore')
 @Controller('explore')
 @UseGuards(AuthenticatedGuard)
 export class ExploreController {
-  constructor(
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    private readonly queryBus: QueryBus,
-  ) {}
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   @Get()
   @Message('Sucessfully fetched posts.')
   @HttpCode(HttpStatus.OK)
-  public async fetchPosts(): Promise<PostDocument[]> {
-    const cachedPosts = await this.cacheManager.get<PostDocument[]>(EXPLORE_CACHE_KEY);
-
-    if (cachedPosts) {
-      return cachedPosts;
-    }
-
-    //bu yapı biraz mantıksız geldi incele.
-    const posts = await this.queryBus.execute(new ExploreCommand());
-
-    await this.cacheManager.set(EXPLORE_CACHE_KEY, posts, 600);
-
-    return posts;
+  public async fetchPosts(): Promise<any> {
+    return this.cacheManager.get(EXPLORE_CACHE_KEY);
   }
 }
