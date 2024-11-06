49b26ec84cb67c3ab66195e090ed4ad5d2c3bee3
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-06 21:08:54 +0300

    refactor(backend): remove unused imports

--- backend/src/features/user/controllers/user.controller.ts
+++ backend/src/features/user/controllers/user.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiTags } from '@nestjs/swagger';
 import { Message } from 'src/common/decorators';
-import { SearchUserDto } from '../dto';
 import { UserDocument } from '../schemas';
 import { SearchUserCommand } from '../command/search-user.command';
 
