60f3cd0c7bc6b57391e6a711b1ec456241eca5d0
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-08 17:30:03 +0300

    fix(backend): add logger to gateway instance

--- backend/src/modules/websocket/gateway.instance.ts
+++ backend/src/modules/websocket/gateway.instance.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Server } from 'socket.io';
 
 @Injectable()
 export class GatewayInstance {
+  private logger = new Logger(GatewayInstance.name);
   private _server: Server;
 
   public set server(server: Server) {
+    this.logger.log('Global io server initalized.');
     this._server = server;
   }
 
