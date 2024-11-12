664cdc2875f229292575d3b3bea4b2ba853cbef1
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-12 15:01:29 +0300

    refactor(backend): add missing return type

--- backend/src/common/decorators/message.decorator.ts
+++ backend/src/common/decorators/message.decorator.ts
@@ -1,7 +1,7 @@
-import { SetMetadata } from '@nestjs/common';
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
 
 export const MESSAGE = 'message' as const;
 
-export const Message = (message: string) => {
+export const Message = (message: string): CustomDecorator<string> => {
   return SetMetadata(MESSAGE, message ?? 'The operation was sucessful.');
 };
